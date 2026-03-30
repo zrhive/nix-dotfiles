c4b709a35de789d98be859b19537bc30aae3d7d7
zhyie <128297362+zhyie@users.noreply.github.com>
2026-03-30 18:03:16 +0800

    refractor: dunst service

--- modules/home/services/dunst/default.nix
+++ modules/home/services/dunst/default.nix
@@ -1,13 +1,48 @@
-{ pkgs, inputs, ... }:
 {
-  services.dunst = {
-    enable = true;
-    iconTheme = {
-      package = pkgs.papirus-icon-theme;
-      name = "Papirus";
-      size = "32x32";
-    };
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+let
+  inherit (config.lib.file) mkOutOfStoreSymlink;
+  inherit (config.home) homeDirectory;
 
-    configFile = inputs.dotfiles + "/dunst/dunstrc";
+  dunst = pkgs.dunst;
+in
+{
+  # services.dunst = {
+  #   enable = true;
+  #   iconTheme = {
+  #     package = pkgs.papirus-icon-theme;
+  #     name = "Papirus";
+  #     size = "32x32";
+  #   };
+  #
+  #   configFile = inputs.dotfiles + "/dunst/dunstrc";
+  # };
+
+  xdg.configFile.dunst = {
+    source = mkOutOfStoreSymlink (homeDirectory + "/.os/dotfiles/dunst");
+  };
+
+  home.packages = [ dunst ];
+  dbus.packages = [ dunst ];
+
+  systemd.user.services.dunst = {
+    Unit = {
+      Description = "Dunst notification daemon";
+      Documentation = [ "man:dunst(1)" ];
+      PartOf = [ "graphical-session.target" ];
+      X-Reload-Triggers = [
+        (inputs.dotfiles + "/dunst/dunstrc")
+      ];
+    };
+    Service = {
+      Type = "dbus";
+      BusName = "org.freedesktop.Notifications";
+      ExecStart = [ "${dunst}/bin/dunst" ];
+      ExecReload = "${dunst}/bin/dunstctl reload";
+    };
   };
 }
