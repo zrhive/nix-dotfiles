f83e6d882f899244e93fffd84da7a7afc9e6419e
zhyie <128297362+zhyie@users.noreply.github.com>
2026-06-04 19:36:52 +0800

    Remove profiles

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,11 +2,6 @@
   elitenix = {
     module = [ ./elitenix ];
     users = [ "zhyie" ];
-    profiles = [
-      "laptop"
-      "graphical"
-      "gaming"
-    ];
     platform = "nixos";
     system = "x86_64-linux";
     stateVersion = "25.11";
@@ -16,7 +11,6 @@
   redroid = {
     module = [ ./redroid ];
     users = [ "cherry" ];
-    profiles = [ ];
     platform = "droid";
     system = "aarch64-linux";
     stateVersion = "24.05";
