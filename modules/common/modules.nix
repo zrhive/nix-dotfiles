82829dd1cc1a5c4ef03551c17c1d6a538963d97c
zhyie <128297362+zhyie@users.noreply.github.com>
2026-07-13 10:45:03 +0800

    Change preconfig modules options to `modules.list`

--- modules/common/modules.nix
+++ modules/common/modules.nix
@@ -1,8 +1,6 @@
-{ lib, ... }:
+{ config, lib, ... }:
 
 let
-  inherit (lib) genAttrs;
-
   moduleList = [
     "desktop"
     "laptop"
@@ -12,45 +10,21 @@ let
     "xserver"
     "wayland"
   ];
-
-  mkEnable = name: { enable = lib.mkEnableOption "Enable ${name} modules."; };
 in
 {
-  options = {
-    modules = genAttrs moduleList mkEnable // {
-      shell.beforeExec = lib.mkOption {
-        type = lib.types.lines;
-        default = "";
-        description = "Commands to run before an exec of shell.";
+  options.modules =
+    lib.genAttrs moduleList (name: {
+      enable = lib.mkEnableOption "Enable ${name} modules.";
+    })
+    // {
+      list = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+        description = "List of pre-configured modules to enable.";
       };
     };
 
-    listModules = lib.mkOption {
-      type = lib.types.listOf lib.types.str;
-      default = [ ];
-      example = lib.literalExpression ''
-        listModules = [
-          "bash"
-          "neovim"
-        ];
-      '';
-      description = ''
-        List of pre-configured modules to enable.
-      '';
-    };
-
-    enableModules = lib.mkOption {
-      type = lib.types.attrsOf lib.types.bool;
-      default = { };
-      example = lib.literalExpression ''
-        enableModules = {
-          bash = true;
-          neovim = true;
-        };
-      '';
-      description = ''
-        Attr of pre-configured modules to enable.
-      '';
-    };
+  config = lib.genAttrs config.modules.modules {
+    enable = true;
   };
 }
